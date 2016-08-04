6362735c17e6e01a65250d9d6cf369c6644c0960
Quesle <eneblue@126.com>
2016-08-04 22:56:21 +0800

    Issue https://github.com/flyingSprite/bijou/issues/6: use `$ionicLoading` to show and hide loading indicator

--- www/js/app.js
+++ www/js/app.js
@@ -91,4 +91,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
 
+})
+
+.constant('$ionicLoadingConfig', {
+  template: 'Default Loading Template ...'
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,26 +1,46 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', ['$scope', 'api', function($scope, api) {
+.controller('DashCtrl', ['$scope', '$ionicLoading', 'api', function($scope, $ionicLoading, api) {
   $scope.websites = {};
 
   $scope.openLink = function(link) {
     window.open(link, '_blank', 'location=yes');
   };
 
+  console.log($ionicLoading);
+
+  $scope.showLoading = function() {
+    $ionicLoading.show().then(function() {
+      // This can so something when todo load.
+      console.log('The loading indicator is now displayed.');
+    });
+  };
+
+  $scope.hideLoading = function() {
+    $ionicLoading.hide().then(function() {
+      // This can so something when done load.
+      console.log('The loading done.');
+    });
+  };
+
   $scope.doRefresh = function(name) {
+    $scope.showLoading();
     api.hotnews().success(function(hotnews) {
       if (hotnews) {
         setHotnewsByWebsite(hotnews, name);
       }
+      $scope.hideLoading();
     }).finally(function() {
       $scope.$broadcast(name);
     });
   };
 
+  $scope.showLoading();
   api.hotnews().success(function(hotnews) {
     if (hotnews) {
       setHotnewsByWebsite(hotnews);
     }
+    $scope.hideLoading();
   });
 
   // api.hotnews(function(hotnews){
